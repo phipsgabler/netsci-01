f9ace317ef04509200d807bb950f9878f0c7c2ed
Philipp Gabler <phips.gabler@gmail.com>
2016-11-15 23:10:20 +0100

    Implement forest fire

--- NetSci01.jl
+++ NetSci01.jl
@@ -1,26 +1,24 @@
 module NetSci01
 
 using LightGraphs, GraphPlot
+using Distributions
 
 export readnetwork, samplenetwork
 
 const line_regex = r"^(\d+)\s(\d+)"
 
-@inline maybe{T}(f::Function, m::T) = f(m)
-@inline maybe(::Function, ::Void) = nothing
-
-@inline function splitwhile(s::AbstractString, predicate::Function)
-    i = 1
-    for c in s
-        if !predicate(c)
-            break
-        end
+# @inline function splitwhile(s::AbstractString, predicate::Function)
+#     i = 1
+#     for c in s
+#         if !predicate(c)
+#             break
+#         end
         
-        i += 1
-    end
+#         i += 1
+#     end
     
-    return s[1:i-1], s[i:end]
-end
+#     return s[1:i-1], s[i:end]
+# end
 
 "Read a space separated file into an (undirected) Graph in an efficient way."
 function readnetwork(filename::String, limit::Number = Inf; fromzero::Bool = false)
@@ -56,7 +54,7 @@ end
 
 
 
-
+"Choose a vertex which has not already been visited"
 @inline function choosestart(translation::Dict{Int, Int}, vertices::Int)
     next = rand(1:vertices)
     while haskey(translation, next)
@@ -66,16 +64,29 @@ end
     return next
 end
 
-"Samples from a network graph using a random walk until n nodes are visited"
-function samplenetwork(graph::Graph, n::Integer)
+function samplenetwork(graph::Graph, n::Integer, method::Symbol = :rw; options...)
+    sampling_method = @eval $(Symbol("samplenetwork_", method))
+    sampling_method(graph, n; options...)
+end
+
+samplenetwork_rw(graph::Graph, n::Integer; options...) =
+    samplenetwork_rj(graph, n; c = 0.0, options...)
+
+
+"Samples from a network graph using a random walk until n nodes are visited, with
+some stochastic jumping"
+function samplenetwork_rj(graph::Graph, n::Integer;
+                          c::Float64 = 0.15, limit_factor::Integer = 100)
     @assert n >= 0
-    
+    @assert 0 <= c <= 1
+    @assert limit_factor > 0
+
     if n == 0
         return Graph()
     end
     
     step = 0
-    walk_limit = round(Int, 100 / log10(n + 1) * n)
+    walk_limit = round(Int, limit_factor / log10(n + 1) * n)
     vertices = nv(graph)
     
     last = rand(1:vertices)
@@ -85,39 +96,88 @@ function samplenetwork(graph::Graph, n::Integer)
     
     node_count = 1
     sample = Graph(n)
-
     
     while node_count < n
-        candidates = neighbors(graph, last)
-        
-        # we chose a component of size 1: reinitialize
-        if isempty(candidates)
-            last = choosestart(translation, vertices)
-            node_count += 1
-            translation[last] = node_count
-            step = 0
-            continue
-        end
-        
-        next = rand(candidates)
-        
-        if !haskey(translation, next)
-            node_count += 1
-            translation[next] = node_count
-        end
-        
-        add_edge!(sample, translation[last], translation[next])
-        
-        if step < walk_limit
+        if rand() > c && !isempty(neighbors(graph, last)) && step < walk_limit
+            # choose random neighbour with probability (1-c)
+            next = rand(neighbors(graph, last))
+            
+            if !haskey(translation, next)
+                node_count += 1
+                translation[next] = node_count
+            end
+            
+            add_edge!(sample, translation[last], translation[next])
             last = next
             step += 1
         else
-            # reinitialize; maybe stuck in component
+            # reinitialize; jump, or got stuck in component, or neighbours were empty
             last = choosestart(translation, vertices)
             node_count += 1
             translation[last] = node_count
             step = 0
         end
+    end
+    
+    return sample
+end
+
+"Forest fire: samples from a network graph using bread-first edge following with decay"
+function samplenetwork_ff(graph::Graph, n::Integer;
+                          p::Float64 = 0.15, limit_factor::Integer = 100)
+    
+#    Leskovec, J. Kleinberg, and C. Faloutsos. Graphs over time: Densification laws, shrinking
+#    diamaters and possible explanations. In ACM SIGKDD , 2005.
+    
+    @assert n >= 0
+    @assert 0 <= p <= 1
+    @assert limit_factor > 0
+
+    if n == 0
+        return Graph()
+    end
+    
+    vertices = nv(graph)
+    
+    translation = Dict{Int, Int}()
+    sizehint!(translation, n)
+    
+    node_count = 0
+    sample = Graph(n)
+    
+    # we can't add too many nodes, because the number of nodes of `sample` is fixed
+    while node_count < n
+        initial = choosestart(translation, vertices)
+        translation[initial] = (node_count += 1)
+
+        burning = [initial]
+        finished = false
+        
+        while !isempty(burning)
+            ignited = Int[]
+            
+            for b in burning
+                for n in neighbors(graph, b)
+                    if rand() > p
+                        continue
+                    elseif !haskey(translation, n)
+                        translation[n] = (node_count += 1)
+                    end
+                    
+                    if !add_edge!(sample, translation[b], translation[n])
+                        finished = true
+                        break
+                    end
+                    push!(ignited, n)
+                end
+
+                if finished
+                    break
+                end
+            end
+            
+            burning = ignited
+        end        
     end
     
     return sample
